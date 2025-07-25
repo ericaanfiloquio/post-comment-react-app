fce2c13358e1cfac2ed423269d908a8afafd9dee
Érica Fernandes Anfilóquio <ericafanfiloquio@gmail.com>
2025-07-24 23:23:35 -0300

    Rebase

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -4,11 +4,17 @@ describe('template spec', () => {
     cy.get('[data-qa="profile-box"]')
     cy.get('[data-qa="comment-input"]').first().type('Test 123')
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('data-qa="comment-text"]').contains('Test 123')
-    cy.get('[data-qa="comment-text"]').each(($element)) ; {
-      if ($element.text() == 'Test 123') {
+    cy.get("[data-qa='comment-text']").contains('Test 123')
+    cy.get("[data-qa='comment-text']").each(($element) => {
+      if ($element.text() === 'Test 123') {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     }
   })
 })
+// Caso de teste:
+// Acessar a página: localhost:5173/
+// Escrever “Test 123” no primeiro campo de comentário
+// Publicar o comentário
+// Verificar se ele aparece na lista de comentários
+// Procurar esse comentário e clicar no botão de deletar correspondente
